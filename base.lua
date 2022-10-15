e4138b03029ff33873a74ea889e342e003c5bd0f
Tylers31 <60413040+Tylers31@users.noreply.github.com>
2022-10-14 21:25:05 -0700

    Update base.lua

--- base.lua
+++ base.lua
@@ -260,11 +260,11 @@ function Kavo.CreateLib(kavName, themeList)
     title.BorderSizePixel = 0
     title.Position = UDim2.new(0.0171428565, 0, 0.344827592, 0)
     title.Size = UDim2.new(0, 204, 0, 8)
-    title.Font = Enum.Font.Gotham
-    title.RichText = true
+    title.Font = Enum.Font.Bangers
+    title.RichText = false
     title.Text = kavName
     title.TextColor3 = Color3.fromRGB(245, 245, 245)
-    title.TextSize = 16.000
+    title.TextSize = 18.000
     title.TextXAlignment = Enum.TextXAlignment.Left
 
     MainSide.Name = "MainSide"
